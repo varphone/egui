1d693d94971674b4d2c07022605abb6d9f28cb87
Varphone Wong <varphone@qq.com>
2025-11-15 13:24:28 +0800

    Add demo for negative desired_width in TextEdit

--- crates/egui_demo_lib/src/demo/text_edit.rs
+++ crates/egui_demo_lib/src/demo/text_edit.rs
@@ -46,6 +46,21 @@ impl crate::View for TextEditDemo {
             ui.label(".");
         });
 
+        ui.separator();
+        ui.label("Singleline text edit that reserves space for a Clear button:");
+        ui.horizontal(|ui| {
+            ui.add(egui::TextEdit::singleline(text).desired_width(-50.0))
+                .on_hover_text("Resize the window to see how the TextEdit resizes.");
+            if ui
+                .add_sized([40.0, 20.0], egui::Button::new("Clear"))
+                .clicked()
+            {
+                text.clear();
+            }
+        });
+
+        ui.separator();
+        ui.label("Multiline text edit with hint text:");
         let output = egui::TextEdit::multiline(text)
             .hint_text("Type something!")
             .show(ui);
